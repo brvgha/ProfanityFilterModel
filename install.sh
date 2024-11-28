429b53230912fc5766d11fb45114a41a9af0ad72
brvgha <brughafitzpatrick@gmail.com>
2024-11-28 19:15:50 +0000

    init

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+pip install --upgrade pip
+python -m venv env
+source env/bin/activate
+pip install tensorflow > /dev/null 2>&1
+echo 'Tensorflow Installed'
+pip install keras > /dev/null 2>&1
+echo 'Keras Installed'
+pip install praw > /dev/null 2>&1
+echo 'Praw Installed'
+pip install scikit-learn > /dev/null 2>&1
+echo 'scikit-learn Installed'
+pip install Pillow > /dev/null 2>&1
+echo 'Pillow Installed'
+pip install pickle > /dev/null 2>&1
+echo 'Pickle Installed'
+pip install python-dotenv > /dev/null 2>&1
+echo 'dotenv Installed'
+pip install pandas > /dev/null 2>&1
+echo 'Pandas Installed'
+mkdir -p ./images
+sudo chmod -R +rw ./images
+sudo chown -R $USER:$USER /images/
